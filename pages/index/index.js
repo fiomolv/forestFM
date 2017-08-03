5f6d8a5ac4bcea219c9608affdf5423a4e2f50d0
ruolan <fionaxrl@gmail.com>
2017-08-03 09:44:26 +0800

    add all files

---
+++ pages/index/index.js
@@ -0,0 +1,23 @@
+//index.js
+//获取应用实例
+var app = getApp();
+var touchStartPos = [];
+Page({
+  data: {
+    motto: 'Hello World 测试',
+    userInfo: {},
+    podcastList: [],
+    hideSearchCancel: true,
+    hideSearchClear: true,
+    searchInputValue: '',
+    searchInputFocus: false
+  },
+  //事件处理函数
+  openno1: function(){
+    console.log("open no1");
+    wx.navigateTo({
+      url: '../no1/no1'
+    })
+  },
+
+})
